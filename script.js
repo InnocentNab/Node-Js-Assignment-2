95810d4fc251d9cf0ce77eb7acfb1a7eae29f79b
Solomon Ogunnsola <solomon.eniola7@gmail.com>
2024-01-19 00:40:20 +0100

    updated the code

--- script.js
+++ script.js
@@ -5,32 +5,32 @@ const behaviour = (req, res) => {
   console.log(req.url);
   if (req.url === "/books" && req.method === "GET") {
     res.write("This is the GET Method for books");
-    res.end();
+    // res.end();
   }
 
   if (req.url === "/books" && req.method === "PUT") {
     res.write("This is the PUT Method for books");
-    res.end();
+    // res.end();
   }
 
   if (req.url === "/books" && req.method === "DELETE") {
     res.write("This is the DELETE Method for books");
-    res.end();
+    // res.end();
   }
 
   if (req.url === "/books/author" && req.method === "GET") {
     res.write("This is the GET Method for books and author");
-    res.end();
+    // res.end();
   }
 
   if (req.url === "/books/author" && req.method === "POST") {
     res.write("This is the POST method for books and author ");
-    res.end();
+    // res.end();
   }
 
   if (req.url === "/books/author" && req.method === "PUT") {
     res.write("This is the PUT method for books and author");
-    res.end();
+    // res.end();
   }
   res.end();
 };
